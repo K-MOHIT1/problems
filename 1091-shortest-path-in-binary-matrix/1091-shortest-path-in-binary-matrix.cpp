542f818c3a25f55abd695cd5aa6e795f51211f24
K-MOHIT1 <132916902+K-MOHIT1@users.noreply.github.com>
2026-07-10 16:21:39 +0530

    Time: 261 ms (5.05%), Space: 82.8 MB (5%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,60 +1,49 @@
 class Solution {
 public:
-//bfs
+    vector<vector<int>> directions{{1,1}, {0,1},{1,0},{0,-1},{-1,0},{-1, -1},{1, -1},{-1, 1}};
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-
-
-
-        vector<vector<int>> dir={{1,1},{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{-1,1},{1,-1}};
-        int m=grid.size();
-        int n=grid[0].size();
-
-        if (grid[0][0] != 0 || grid[m - 1][n - 1] != 0) 
+        int m = grid.size();
+        int n = grid[0].size();
+        if(m == 0 || n == 0 || grid[0][0] != 0)
             return -1;
-
-
-        queue<pair<int,int>> q;
-        q.push({0,0});
-        grid[0][0]=1;
-        int level=1;
-
-        auto issafe = [&](int x,int y){
-            if(x>=0 and x<m and y>=0 and y<n) return true;
-            return false;
+        
+        queue<pair<int, int>> que;
+        que.push({0, 0});
+        grid[0][0] = 1;
+        
+        auto isSafe = [&](int x, int y) {
+            return x>=0 && x<m && y>=0 && y<n;
         };
-
-        while(q.size()){
-
-            int N=q.size();
-
+        
+        int steps = 1;
+        
+        while(!que.empty()) {
             
-            while(N--){
-                auto curr=q.front();
-                q.pop();
+            int N = que.size();
+            while(N--) {
 
+                auto curr = que.front();
+                que.pop();
+                int x = curr.first;
+                int y = curr.second;
 
-                int x=curr.first;
-                int y=curr.second;
+                if(x == m-1 && y == n-1)
+                    return steps;
 
-                if(x==m-1 and y==n-1) return level;
+                for(auto dir:directions) {
+                    int x_ = x + dir[0];
+                    int y_ = y + dir[1];
 
-                for(auto& di:dir){
-                    int x_=x+ di[0];
-                    int y_=y+di[1];
-
-                    if(issafe(x_,y_) and grid[x_][y_]==0){
-                        grid[x_][y_]=1;
-                        q.push({x_,y_});
+                    if(isSafe(x_, y_) && grid[x_][y_] == 0) {
+                        que.push({x_, y_});
+                        grid[x_][y_] = 1;
                     }
                 }
             }
-
-            level++;
-
+            steps++;
         }
-
+        
         return -1;
-
-
     }
 };
+
