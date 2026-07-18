f9f9844218a685d96986596218afe28e3ccfea3b
K Mohit <132916902+K-MOHIT1@users.noreply.github.com>
2026-07-18 15:57:37 +0530

    Time: 2 ms (35.18%), Space: 45.3 MB (79.42%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static void recur(List<List<Integer>> ans, List<Integer> temp, int[] nums){
+        if(temp.size() == nums.length){
+            ans.add(new ArrayList(temp));
+            return;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(temp.contains(nums[i])) continue;
+            else{
+                int ch=nums[i];
+                temp.add(ch);
+                recur(ans, temp, nums);
+                temp.remove(temp.size()-1);
+            }
+            
+        }
+    }
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> tem = new ArrayList<>();
+        recur(ans, tem , nums);
+        return ans;
+    }
+}
