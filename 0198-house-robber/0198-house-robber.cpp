1b6b1352288744e7af3213bba1f4ef8e611b598a
K Mohit <132916902+K-MOHIT1@users.noreply.github.com>
2026-07-19 11:41:19 +0530

    Time: 0 ms (100%), Space: 10.2 MB (78.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,7 +12,20 @@ public:
         return dp[i]=ans;
     }
     int rob(vector<int>& nums) {
-        dp.resize(401,-1);
+        dp.resize(401,0);
+
+        dp[0]=0;
+        dp[1]=nums[0];
+
+        for(int i=2;i<=nums.size();i++){
+            dp[i]=max(dp[i-1],nums[i-1]+dp[i-2]);
+        }
+
+        return dp[nums.size()];
+
+
+
+        
         return recur(nums,0,0);
     }
 };
