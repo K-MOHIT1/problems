af7e5a7fa7d4b7674b369cf83ae5e648b4c3abc8
K Mohit <132916902+K-MOHIT1@users.noreply.github.com>
2026-07-20 05:23:44 +0530

    Time: 78 ms (47.46%), Space: 14.4 MB (48.75%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,29 +1,21 @@
 class Solution {
 public:
-    int dp[2501][2502];
-
-    int recur(vector<int>& nums, int i, int prev) {
-        if (i >= nums.size())
-            return 0;
-
-        if (prev!=-1 and dp[i][prev] != -1)
-            return dp[i][prev];
-
-        int skip = recur(nums, i + 1, prev);
-
-        int take = 0;
-        if (prev == -1 || nums[i] > nums[prev]) {
-            take = 1 + recur(nums, i + 1, i);
-        }
-
-        if(prev!=-1){
-            dp[i][prev]=max(skip, take);
-        }
-        return max(skip, take);
-    }
-
     int lengthOfLIS(vector<int>& nums) {
-        memset(dp, -1, sizeof(dp));
-        return recur(nums, 0, -1);
+        int n=nums.size();
+        vector<int> t(n,1);
+
+        int ans=1;
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    t[i]=max(t[i],t[j]+1);
+                    ans=max(ans,t[i]);
+
+                }
+            }
+        }
+
+        return ans;
     }
 };
