fd52b131a10809cd746c7c6938bc1ddf881f0fa1
K Mohit <132916902+K-MOHIT1@users.noreply.github.com>
2026-07-24 16:44:16 +0530

    Time: 0 ms (100%), Space: 14.1 MB (87.57%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+
+//patience sorting
+
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<int> t(n,1);
+        vector<int> sorted;
 
-        int ans=1;
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    t[i]=max(t[i],t[j]+1);
-                    ans=max(ans,t[i]);
+            
+            auto it=lower_bound(begin(sorted),end(sorted),nums[i]);
 
-                }
+            if(it == end(sorted)){
+                sorted.push_back(nums[i]);
+            }
+            else{
+                *it = nums[i];
             }
         }
 
-        return ans;
+        return sorted.size();
     }
 };
